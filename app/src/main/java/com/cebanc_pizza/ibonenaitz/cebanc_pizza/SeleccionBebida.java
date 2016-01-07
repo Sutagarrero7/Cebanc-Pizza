faa75b6f6a80869be8df56511acd0289e00964ec
Sutagarrero7 <ensagasta@gmail.com>
2016-01-07 10:03:24 +0100

    C

--- app/src/main/java/com/cebanc_pizza/ibonenaitz/cebanc_pizza/SeleccionBebida.java
+++ app/src/main/java/com/cebanc_pizza/ibonenaitz/cebanc_pizza/SeleccionBebida.java
@@ -2,6 +2,10 @@ package com.cebanc_pizza.ibonenaitz.cebanc_pizza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class SeleccionBebida extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class SeleccionBebida extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_bebida);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(SeleccionBebida.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
