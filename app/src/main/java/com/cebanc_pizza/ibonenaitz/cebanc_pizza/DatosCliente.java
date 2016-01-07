e3b37154844997868aa2c8cfb26d578d9a29c271
Sutagarrero7 <ensagasta@gmail.com>
2016-01-07 08:31:46 +0100

    C

--- app/src/main/java/com/cebanc_pizza/ibonenaitz/cebanc_pizza/DatosCliente.java
+++ app/src/main/java/com/cebanc_pizza/ibonenaitz/cebanc_pizza/DatosCliente.java
@@ -26,7 +26,6 @@ public class DatosCliente extends AppCompatActivity {
                                             @Override
                                             public void onClick(View v) {
                                                 finish();
-
                                             }
                                         }
         );
